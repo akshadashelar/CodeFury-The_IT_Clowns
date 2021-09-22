05807e941002e7b866c87a29a4a5a8865db660ec
Siddhant Touti <siddhanttouti@gmail.com>
2021-09-22 22:37:47 +0530

    Update: Database dump file, contains dummy values

---
+++ order_processing_system_dump_2.sql
@@ -0,0 +1,234 @@
+CREATE DATABASE  IF NOT EXISTS `order_processing_system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `order_processing_system`;
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: order_processing_system
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_company`
+--
+
+DROP TABLE IF EXISTS `tbl_company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_company` (
+  `company_id` int NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(255) DEFAULT NULL,
+  `company_address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `gst_number` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`company_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_company`
+--
+
+LOCK TABLES `tbl_company` WRITE;
+/*!40000 ALTER TABLE `tbl_company` DISABLE KEYS */;
+INSERT INTO `tbl_company` VALUES (1,'HSBC','Business Bay, Pune','Pune','101'),(2,'Apple','UB City, 19th Floor, Concorde Tower C, Vittal Mallya Rd, Bengaluru, 560001','Bengaluru','201'),(3,'BoAt','2nd Floor, 19, 16, Hauz Khas Village, Deer Park, Hauz Khas, New Delhi 110016','New Delhi','301');
+/*!40000 ALTER TABLE `tbl_company` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_customer`
+--
+
+DROP TABLE IF EXISTS `tbl_customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `gst_number` varchar(255) DEFAULT NULL,
+  `pincode` bigint DEFAULT NULL,
+  `mobile_number` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_customer`
+--
+
+LOCK TABLES `tbl_customer` WRITE;
+/*!40000 ALTER TABLE `tbl_customer` DISABLE KEYS */;
+INSERT INTO `tbl_customer` VALUES (1,'Oshee Gupta','oshee@123','HSBC Pune','oshee@gmail.com','Pune','20',411001,'1234567890'),(2,'Muskan Gupta','muskan@123','HSBC Pune','muskan@gmail.com','Pune','30',411001,'1234567890'),(3,'Shaunak Padhye','shaunak@123','HSBC Pune','shaunak@gmail.com','Pune','31',411001,'1234567890');
+/*!40000 ALTER TABLE `tbl_customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_employee`
+--
+
+DROP TABLE IF EXISTS `tbl_employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_employee` (
+  `employee_id` int NOT NULL AUTO_INCREMENT,
+  `employee_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_employee`
+--
+
+LOCK TABLES `tbl_employee` WRITE;
+/*!40000 ALTER TABLE `tbl_employee` DISABLE KEYS */;
+INSERT INTO `tbl_employee` VALUES (1,'Akshada Shelar','akshada'),(2,'Siddhant Touti','siddhant'),(3,'Mayur Naik','mayur');
+/*!40000 ALTER TABLE `tbl_employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_invoice`
+--
+
+DROP TABLE IF EXISTS `tbl_invoice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_invoice` (
+  `invoice_id` int NOT NULL AUTO_INCREMENT,
+  `invoice_date` date DEFAULT NULL,
+  `order_id` int DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `gst_type` varchar(255) DEFAULT NULL,
+  `total_value` decimal(10,0) DEFAULT NULL,
+  `status` enum('Paid','Unpaid') DEFAULT NULL,
+  PRIMARY KEY (`invoice_id`),
+  KEY `order_id` (`order_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `tbl_invoice_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `tbl_order` (`order_id`),
+  CONSTRAINT `tbl_invoice_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `tbl_customer` (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_invoice`
+--
+
+LOCK TABLES `tbl_invoice` WRITE;
+/*!40000 ALTER TABLE `tbl_invoice` DISABLE KEYS */;
+INSERT INTO `tbl_invoice` VALUES (1,'2021-09-01',2,2,'inter-state',275,'Paid'),(2,'2021-09-20',6,2,'same state',660,'Paid'),(3,'2021-09-21',5,1,'same state',550,'Unpaid');
+/*!40000 ALTER TABLE `tbl_invoice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_order`
+--
+
+DROP TABLE IF EXISTS `tbl_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_order` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `order_date` date DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `customer_shipping_address` varchar(255) DEFAULT NULL,
+  `total_order_value` decimal(10,0) DEFAULT NULL,
+  `shipping_cost` decimal(10,0) DEFAULT NULL,
+  `shipping_agency` varchar(255) DEFAULT NULL,
+  `status` enum('Pending','Approved','Completed','Expired') DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `tbl_order_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `tbl_customer` (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+LOCK TABLES `tbl_order` WRITE;
+/*!40000 ALTER TABLE `tbl_order` DISABLE KEYS */;
+INSERT INTO `tbl_order` VALUES (1,'2021-09-21',1,'customer shipping address 1',100,10,'bluedart','Pending'),(2,'2021-09-02',2,'customer shipping address 2',250,25,'bluedart','Completed'),(3,'2021-09-21',1,'customer shipping address 3',300,30,'bluedart','Pending'),(4,'2021-09-21',2,'customer shipping address 4',400,450,'bluedart','Pending'),(5,'2021-09-21',1,'customer shipping address 5',500,50,'bluedart','Approved'),(6,'2021-09-21',2,'customer shipping address 6',600,60,'bluedart','Completed');
+/*!40000 ALTER TABLE `tbl_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_orderhasproducts`
+--
+
+DROP TABLE IF EXISTS `tbl_orderhasproducts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_orderhasproducts` (
+  `order_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int DEFAULT '1',
+  PRIMARY KEY (`order_id`,`product_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `tbl_orderhasproducts_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `tbl_order` (`order_id`),
+  CONSTRAINT `tbl_orderhasproducts_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `tbl_product` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_orderhasproducts`
+--
+
+LOCK TABLES `tbl_orderhasproducts` WRITE;
+/*!40000 ALTER TABLE `tbl_orderhasproducts` DISABLE KEYS */;
+INSERT INTO `tbl_orderhasproducts` VALUES (1,1,1),(2,1,1),(3,6,2),(4,3,1);
+/*!40000 ALTER TABLE `tbl_orderhasproducts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_product`
+--
+
+DROP TABLE IF EXISTS `tbl_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_product` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `category` enum('level_1','level_2','level_3') DEFAULT NULL,
+  `company_id` int DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `tbl_product_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `tbl_company` (`company_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+LOCK TABLES `tbl_product` WRITE;
+/*!40000 ALTER TABLE `tbl_product` DISABLE KEYS */;
+INSERT INTO `tbl_product` VALUES (1,'Headphones',100,'level_1',3),(2,'iPhone',1000,'level_2',2),(3,'iPad',600,'level_2',2),(4,'Debit Card',5,'level_3',1),(5,'Credit Card',5,'level_3',1),(6,'Earbuds',150,'level_1',3);
+/*!40000 ALTER TABLE `tbl_product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-22 22:35:17
