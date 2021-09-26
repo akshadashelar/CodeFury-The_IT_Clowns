5edc30c79d3ccc5dad25c212ba2dab2c62b44cf4
Siddhant Touti <siddhanttouti@gmail.com>
2021-09-26 13:55:30 +0530

    Update: Database Dump

--- order_processing_system_dump_3.sql
+++ order_processing_system_dump_3.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `order_processing_system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `order_processing_system`;
 -- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
 --
 -- Host: localhost    Database: order_processing_system
@@ -112,7 +110,7 @@ CREATE TABLE `tbl_invoice` (
   `invoice_date` date DEFAULT NULL,
   `order_id` int DEFAULT NULL,
   `customer_id` int DEFAULT NULL,
-  `gst_type` varchar(255) DEFAULT NULL,
+  `gst_type` enum('INTER_STATE','SAME_STATE') DEFAULT NULL,
   `total_value` decimal(10,0) DEFAULT NULL,
   `status` enum('Paid','Unpaid') DEFAULT NULL,
   PRIMARY KEY (`invoice_id`),
@@ -129,7 +127,7 @@ CREATE TABLE `tbl_invoice` (
 
 LOCK TABLES `tbl_invoice` WRITE;
 /*!40000 ALTER TABLE `tbl_invoice` DISABLE KEYS */;
-INSERT INTO `tbl_invoice` VALUES (1,'2021-09-01',2,2,'inter-state',275,'Paid'),(2,'2021-09-20',6,2,'same state',660,'Paid'),(3,'2021-09-21',5,1,'same state',550,'Unpaid');
+INSERT INTO `tbl_invoice` VALUES (1,'2021-09-01',2,2,'INTER_STATE',275,'Paid'),(2,'2021-09-20',6,2,'SAME_STATE',660,'Paid'),(3,'2021-09-21',5,1,'SAME_STATE',550,'Unpaid');
 /*!40000 ALTER TABLE `tbl_invoice` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,7 +159,7 @@ CREATE TABLE `tbl_order` (
 
 LOCK TABLES `tbl_order` WRITE;
 /*!40000 ALTER TABLE `tbl_order` DISABLE KEYS */;
-INSERT INTO `tbl_order` VALUES (1,'2021-09-21',1,'customer shipping address 1',100,10,'bluedart','Pending'),(2,'2021-09-02',2,'customer shipping address 2',250,25,'bluedart','Completed'),(3,'2021-09-21',1,'customer shipping address 3',300,30,'bluedart','Pending'),(4,'2021-09-21',2,'customer shipping address 4',400,450,'bluedart','Pending'),(5,'2021-09-21',1,'customer shipping address 5',500,50,'bluedart','Approved'),(6,'2021-09-21',2,'customer shipping address 6',600,60,'bluedart','Completed');
+INSERT INTO `tbl_order` VALUES (1,'2021-09-21',1,'customer shipping address 1',100,10,'bluedart','Completed'),(2,'2021-09-02',2,'customer shipping address 2',250,25,'bluedart','Completed'),(3,'2021-09-21',1,'customer shipping address 3',300,30,'bluedart','Pending'),(4,'2021-09-21',2,'customer shipping address 4',400,450,'bluedart','Pending'),(5,'2021-09-21',1,'customer shipping address 5',500,50,'bluedart','Approved'),(6,'2021-09-21',2,'customer shipping address 6',600,60,'bluedart','Completed');
 /*!40000 ALTER TABLE `tbl_order` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -204,7 +202,7 @@ CREATE TABLE `tbl_product` (
   `product_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `price` decimal(10,0) DEFAULT NULL,
-  `category` enum('level_1','level_2','level_3') DEFAULT NULL,
+  `category` int DEFAULT NULL,
   `company_id` int DEFAULT NULL,
   PRIMARY KEY (`product_id`),
   KEY `company_id` (`company_id`),
@@ -218,7 +216,7 @@ CREATE TABLE `tbl_product` (
 
 LOCK TABLES `tbl_product` WRITE;
 /*!40000 ALTER TABLE `tbl_product` DISABLE KEYS */;
-INSERT INTO `tbl_product` VALUES (1,'Headphones',100,'level_1',3),(2,'iPhone',1000,'level_2',2),(3,'iPad',600,'level_2',2),(4,'Debit Card',5,'level_3',1),(5,'Credit Card',5,'level_3',1),(6,'Earbuds',150,'level_1',3);
+INSERT INTO `tbl_product` VALUES (1,'Headphones',100,1,3),(2,'iPhone',1000,2,2),(3,'iPad',600,2,2),(4,'Debit Card',5,3,1),(5,'Credit Card',5,3,1),(6,'Earbuds',150,1,3);
 /*!40000 ALTER TABLE `tbl_product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -231,4 +229,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-22 22:35:17
+-- Dump completed on 2021-09-26 13:53:04
